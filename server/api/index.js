b438163911f970d03553dfa7a92e3ced3f12671f
Tim Ninan <tim.ninan@gmail.com>
2017-12-15 15:27:23 -0600

    axios PUT request call server endpoint detected

--- server/api/index.js
+++ server/api/index.js
@@ -140,6 +140,14 @@ router.post(
       .catch(err => res.send(err));
   },
 );
+//update a playlist endpoint
+router.put(
+  '/aplaylist',
+  (req, res) => {
+    console.log('PUT to /aplaylist detected!');
+    console.log('req.body is ', req.body);
+  },
+);
 
 router.post(
   '/getFollowing',
